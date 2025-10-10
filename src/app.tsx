89819ca153abb2fd9a469c5d907033ca58d7bf77
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-10-10 10:07:05 +0700

    feat: add button button shadcn

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App(){
   return(
     <div >
       <h1 className="p-4 border bg-blue-600">Seduh.in</h1>
-      <button>Click me</button>
+      <Button>Click me</Button>
     </div>
   )
 }
