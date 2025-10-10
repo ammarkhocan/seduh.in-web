ce802666f92b03df2c279c0f497f27dbd9531200
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-10-10 13:18:55 +0700

    chore: update dependencies and improve code formatting

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
 import { Button } from "./components/ui/button";
 
-export function App(){
-  return(
-    <div >
-      <h1 className="p-4 border bg-blue-600">Seduh.in</h1>
+export function App() {
+  return (
+    <div>
+      <h1 className="border bg-blue-600 p-4">Seduh.in</h1>
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
