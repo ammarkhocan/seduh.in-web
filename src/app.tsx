42a136bcfd17df940388204474d825d87c07c9dd
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-10-10 09:51:33 +0700

    style: update App component styling and remove unused CSS

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 export function App(){
   return(
-    <div>
-      <h1>Seduh.in</h1>
+    <div >
+      <h1 className="p-4 border bg-blue-600">Seduh.in</h1>
       <button>Click me</button>
     </div>
   )
