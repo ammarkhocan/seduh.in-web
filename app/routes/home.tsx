e96e7f215606ca49a418e959815e73accb545b58
ammarkhocan <ammarkhocan02@gmail.com>
2025-10-21 20:56:30 +0700

    refactor: remove unused Welcome component and associated assets

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: "Seduh.in" }, { name: "description", content: "Coffe from the Seduh.in" }];
+}
+
+export async function clientLoader() {
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/products`);
+  const products = await response.json();
+  return { products };
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <h1>Seduh.in</h1>
+    </div>
+  );
 }
