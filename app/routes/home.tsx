abdb99219a2d0e8e66c61efbdd3cbd52c879b8cf
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-21 21:04:31 +0700

    day 37

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,19 +1,27 @@
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "Seduh.in" }, { name: "description", content: "Coffe from the Seduh.in" }];
+  return [
+    { title: "Seduh.in" },
+    { name: "description", content: "Coffe from the Seduh.in" },
+  ];
 }
 
 export async function clientLoader() {
-  const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/products`);
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/products`
+  );
   const products = await response.json();
   return { products };
 }
 
-export default function Home() {
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { products } = loaderData;
+
   return (
     <div>
       <h1>Seduh.in</h1>
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </div>
   );
 }
