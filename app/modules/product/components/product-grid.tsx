c4ae40eb6613c807b2c93a14aee7f091d13f2627
ammarkhocan <ammarkhocan02@gmail.com>
2025-10-24 17:16:53 +0700

    feat: link product slug

--- app/modules/product/components/product-grid.tsx
+++ app/modules/product/components/product-grid.tsx
@@ -1,4 +1,5 @@
 import { ShoppingBag } from "lucide-react";
+import { Link } from "react-router";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { formatPrice } from "~/lib/format";
@@ -9,20 +10,24 @@ export function ProductsGrid({ products }: { products: Products }) {
     <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
       {products.map((product) => (
         <li key={product.id}>
-          <Card className="hover:shadow-lg transition-shadow">
-            <img src={product.imageUrl} alt={product.name} className="w-full h-80 object-cover rounded-t-lg" />
-            <CardHeader>
-              <CardTitle>{product.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm text-muted-foreground mb-2">Stok: {product.stock}</p>
-              <p className="text-lg font-bold text-stone-800 dark:text-stone-300 mb-4">{formatPrice(product.price)}</p>
-              <Button className="w-full bg-stone-800 hover:bg-stone-900 dark:bg-stone-700 dark:hover:bg-stone-800">
-                <ShoppingBag className="w-4 h-4 mr-2" />
-                Add to cart
-              </Button>
-            </CardContent>
-          </Card>
+          <Link to={`/products/${product.slug}`}>
+            <Card className="hover:shadow-lg transition-shadow">
+              <img src={product.imageUrl} alt={product.name} className="w-full h-80 object-cover rounded-t-lg" />
+              <CardHeader>
+                <CardTitle>{product.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm text-muted-foreground mb-2">Stok: {product.stock}</p>
+                <p className="text-lg font-bold text-stone-800 dark:text-stone-300 mb-4">
+                  {formatPrice(product.price)}
+                </p>
+                <Button className="w-full bg-stone-800 hover:bg-stone-900 dark:bg-stone-700 dark:hover:bg-stone-800">
+                  <ShoppingBag className="w-4 h-4 mr-2" />
+                  Add to cart
+                </Button>
+              </CardContent>
+            </Card>
+          </Link>
         </li>
       ))}
     </ul>
