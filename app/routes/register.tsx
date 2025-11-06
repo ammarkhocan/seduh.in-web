aaf49ae5d0c1c48402a9f50c3a3d6c71ec9d2b5c
ammarkhocan <ammarkhocan02@gmail.com>
2025-11-06 07:27:29 +0700

    feat: register form and add new label component

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,4 +1,8 @@
+import { Form } from "react-router";
 import type { Route } from "./+types/register";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Register" }];
@@ -8,6 +12,26 @@ export default function RegisterRoute({}: Route.ComponentProps) {
   return (
     <div>
       <h1>Create new account</h1>
+
+      <Form>
+        <div>
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" type="text" name="username" />
+        </div>
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" name="email" />
+        </div>
+        <div>
+          <Label htmlFor="full-name">Full Name</Label>
+          <Input id="full-name" type="text" name="fullName" />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" name="password" />
+        </div>
+        <Button type="submit">Create New Account</Button>
+      </Form>
     </div>
   );
 }
