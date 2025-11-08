e27aa5ed64c6d317e312c2e77f146d6c51f15b6b
ammarkhocan <ammarkhocan02@gmail.com>
2025-11-08 13:04:13 +0700

    feat: dashboard ui and handle expired

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -3,6 +3,8 @@ import Cookies from "js-cookie";
 import type { MeResponse } from "~/modules/user/type";
 import type { Route } from "./+types/dashboard";
 import { redirect } from "react-router";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
+import { User } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Dashboard" }];
@@ -18,6 +20,11 @@ export async function clientLoader() {
     headers: { Authorization: `Bearer ${token}` },
   });
 
+  if (!response.ok) {
+    Cookies.remove("token");
+    return redirect("/login");
+  }
+
   const meResponse: MeResponse = await response.json();
 
   return { meResponse };
@@ -27,9 +34,39 @@ export default function HomeRoute({ loaderData }: Route.ComponentProps) {
   const { meResponse } = loaderData;
 
   return (
-    <div className="min-h-screen bg-background py-8">
-      <h1>Dashboard</h1>
-      <pre>{JSON.stringify(meResponse, null, 2)}</pre>
+    <div className="min-h-screen bg-gray-50 py-12 px-6">
+      <div className="max-w-2xl mx-auto">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">Dashboard</h1>
+        <p>Your account overview</p>
+
+        <Card>
+          <CardHeader>
+            <CardTitle className="mt-9 text-center">Profile</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="grid grid-cols-2 gap-8">
+              <div>
+                <span className="text-gray-600 block mb-1">User ID</span>
+                <span className="font-medium text-gray-900">{meResponse.id}</span>
+              </div>
+              <div>
+                <span className="text-gray-600 block mb-1">Full Name</span>
+                <span className="font-medium text-gray-900">{meResponse.fullName}</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-8">
+              <div>
+                <span className="text-gray-600 block mb-1">Username</span>
+                <span className="font-medium text-gray-900">{meResponse.username}</span>
+              </div>
+              <div>
+                <span className="text-gray-600 block mb-1">Email</span>
+                <span className="font-medium text-gray-900">{meResponse.email}</span>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
