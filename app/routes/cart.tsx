a47d1ff963c5facf66bdf0f2e1ac94184d4e85ce
M Haidar Hanif <git@mhaidarhanif.com>
2025-11-11 20:31:12 +0700

    day 40

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -2,16 +2,13 @@ import Cookies from "js-cookie";
 import type { MeResponse } from "~/modules/user/type";
 import type { Route } from "./+types/cart";
 import { redirect } from "react-router";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Cart" }];
 }
 
 export async function clientLoader() {
-  //Token
   const token = Cookies.get("token");
-
   if (!token) return redirect("/login");
 
   const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/cart`, {
@@ -23,14 +20,12 @@ export async function clientLoader() {
     return redirect("/login");
   }
 
-  const meResponse: MeResponse = await response.json();
+  const cart: { id: string } = await response.json();
 
-  return { meResponse };
+  return { cart };
 }
 
 export default function CartRoute({ loaderData }: Route.ComponentProps) {
-  const { meResponse } = loaderData;
-
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-3xl font-bold mb-8">Shoping Cart</h1>
