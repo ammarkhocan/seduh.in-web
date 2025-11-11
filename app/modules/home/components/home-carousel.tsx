72972130e09dcba2b43d057847cc379b22cfec57
ammarkhocan <ammarkhocan02@gmail.com>
2025-11-11 07:54:39 +0700

    feat: remove comment

--- app/modules/home/components/home-carousel.tsx
+++ app/modules/home/components/home-carousel.tsx
@@ -5,7 +5,6 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 export function HomeCarousel() {
   const plugin = React.useRef(Autoplay({ delay: 3000, stopOnInteraction: true }));
 
-  // Daftar gambar dari folder public/images
   const images = [
     "https://1ea7bnszm7.ucarecd.net/f31477da-2ac7-4c0f-bfbb-97a0796faed0/-/preview/1000x391",
     "https://1ea7bnszm7.ucarecd.net/29703a54-029f-4cc1-9101-6ab41971b784/-/preview/1000x390",
